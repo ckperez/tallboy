c9b03e73957945201f5b1406cd0201255f6b7409
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-05-21 11:30:54 -0700

    filled in some more scaffolding in router.js

--- index.js
+++ index.js
@@ -3,4 +3,8 @@
 const server = require(__dirname + '/lib/server.js');
 const router = require(__dirname + '/lib/router.js');
 
+var handle = {};
+
+handle['/'] = requestHandlers.crack;
+
 server.crack(router.route, handle);
